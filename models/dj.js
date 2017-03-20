10f0452acc292120165ad06c8aa417c6857e0184
Anton Bogatskiy <holytony@MacBook-Pro-HolyTony.local>
2017-03-20 22:13:16 +0300

    add mixcloud

---
+++ models/dj.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const DjSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Не указано имя Dj']
+  },
+  img: {
+    type: String,
+    required: [true, 'Не указан img']
+  },
+  city: {
+    type: String,
+    required: [true, 'Не указано город Dj']
+  },
+  desc: {
+    type: String,
+    required: [true, 'Добавте описание']
+  },
+  sets: {
+    type: Array
+  }
+})
+
+mongoose.model('dj', DjSchema)
--- routes/music.js
+++ routes/music.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose')
 
-router.get('/', function(req, res) {
-  res.render('pages/music', {page: 'Музыка'});
+// router.get('/', function (req, res) {
+//   let obj = {page: 'Музыка'}
+//   const Model = mongoose.model('dj')
+//
+//   Model.find().then(djs => {
+//     Object.assign(obj, {djs: djs})
+//     res.render('pages/music', obj);
+//   })
+// });
+
+router.get('/', function (req, res) {
+  let obj = {page: 'Музыка'}
+
+  res.render('pages/music', obj);
+});
+
+
+router.post('/', (req, res) => {
+  if (!req.body.name || !req.body.city || !req.body.desc) {
+    return res.json({status: 'no data'})
+  }
+  const Model = mongoose.model('dj')
+  let item = new Model({
+    name: req.body.name,
+    img: req.body.img,
+    city: req.body.city,
+    desc: req.body.desc
+  })
+  item.save().then(
+    i => {
+      return res.json({status: 'saved'});
+    },
+    e => {
+      const error = Object
+        .keys(e.error)
+        .map(key => e.errors[key].message)
+        .join(', ')
+      res.json(error)
+    }
+  )
 });
 
 module.exports = router;
