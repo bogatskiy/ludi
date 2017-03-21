b43f8ec3debff02d7bf9ff2716c0a916d9805cf0
Anton Bogatskiy <holytony@MacBook-Pro-HolyTony.local>
2017-03-21 19:55:28 +0300

    use strict fix bug

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(function (err, req, res, next) {
 });
 
 
-server.listen(80, '188.166.95.105');
+server.listen(80, '207.154.227.160');
 // server.listen(3000, 'localhost');
 server.on('listening', function () {
   if (!fs.existsSync(uploadDir)) {
